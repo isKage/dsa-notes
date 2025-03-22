09eb326c315cfaa755a27b51aaa6bbb7b1341237
isKage <kageshu211@gmail.com>
2025-03-22 10:21:41 +0800

    update Assignments/HW1: a new solution to n_queens_problem

--- Assignments/HW1/hw1_8_nqueens.py
+++ Assignments/HW1/hw1_8_nqueens.py
@@ -44,50 +44,38 @@ def n_queens_stack(N):
     """
     N 皇后问题
     :param N: 棋盘大小 N x N
-    :return: 结果列表 res = []
+    :return: 结果列表 res = [] 存储了转换格式后的解
     """
     res = []  # 存储所有有效解
-    stack = ArrayStack()  # 存储每一行的信息
+    stack = ArrayStack()  # 栈存储皇后的位置（一个列表，元素为位置坐标）
+    stack.push([])  # 初始化
+    # 以列表 [] 形式存储是为了简便检索之前已经确定的皇后位置，否则需要遍历栈元素：以空间换时间
 
-    # 初始状态：queen 的位置，第 0 行，第一个 set 表示列，第二个表示主对角线，第三个表示副对角线
-    # set() 存储了之前放入皇后的位置，表示已被占有，后面的行皇后不可填入这些列
-    stack.push(([], 0, set(), set(), set()))
-
-    while not stack.is_empty():
+    while not stack.is_empty():  # 空栈则无解
         # Step 1: 出栈当前状态
-        queen, row, cols, diag1, diag2 = stack.pop()
+        queens = stack.pop()
+        row = len(queens)  # 列表长度代表已经放置的皇后数
 
-        # 如果所有行都处理完毕，说明 queen 是一个有效解
+        # Step 2: 如果所有行都处理完毕，说明 queens 是一个有效解列表
         if row == N:
             # 转换为棋盘表示
-            res.append([' · ' * i + ' Q ' + ' · ' * (N - i - 1) for i in queen])
+            res.append([' · ' * i + ' Q ' + ' · ' * (N - i - 1) for i in [q[1] for q in queens]])
             continue
 
-        # Step 2: 在当前行选择列的位置，即 queen 位置
+        # Step 3: 在当前行选择确切位置，即向 queens 列表加入下一行新的位置
         for col in range(N):
-            # 检查 主对角线、副对角线 是否有其他皇后
-            curr_diag1 = row - col  # 正对角线：行 - 列
-            curr_diag2 = row + col  # 反对角线：行 + 列
+            flag = True  # 判断是否可填入
+            for r, c in queens:  # 检查之前的所有皇后位置
+                if c == col or abs(r - row) == abs(c - col):
+                    flag = False  # 同列、同对角线 -> False
 
-            # 列、主对角线、副对角线 是否被占用，占有则跳过，继续寻找
-            if col in cols or curr_diag1 in diag1 or curr_diag2 in diag2:
-                continue
+            if flag:
+                # 如果可填入则加入 queens 位置解列表
+                new_queens = queens + [(row, col)]  # 需要复制一份，以免改变栈里其他元素
+                # 压入新状态：更新皇后位置
+                stack.push(new_queens)
 
-            # 创建新集合（创建副本，以免相互干扰，否则会直接改变栈中其他元素的值）
-            new_cols = set(cols)  # 创建新的列集
-            new_diag1 = set(diag1)  # 创建新的主对角线集
-            new_diag2 = set(diag2)  # 创建新的副对角线集
-
-            # 更新
-            new_cols.add(col)  # 将当前列加入列 new_cols 集，表示占有
-            new_diag1.add(curr_diag1)  # 将当前正对角线加入冲突集，表示占有
-            new_diag2.add(curr_diag2)  # 将当前反对角线加入冲突集，表示占有
-            new_queen = queen + [col]  # 将当前列加入路径，表示占有
-
-            # 压入新状态：更新皇后位置、下一行、列、对角线
-            stack.push((new_queen, row + 1, new_cols, new_diag1, new_diag2))
-
-    return res, len(res)
+    return res, len(res)  # 返回解和解个数
 
 
 if __name__ == '__main__':
